f92d685e88a13640c6fe0f0d02e88c8732175bb3
Jemu <60427577+Jemu129@users.noreply.github.com>
2020-12-08 15:36:45 +0200

    Update sketch_oct27a.ino

--- sketch_oct27a.ino
+++ sketch_oct27a.ino
@@ -1,13 +1,63 @@
-#define soilSensor A0
+int sensorPin = A0;
+int sensorValue;
+int limit = 300;
 
-void setup() {
-  // put your setup code here, to run once:
+void setup() 
+{
   Serial.begin(9600);
+  pinMode(13, OUTPUT);int sensorPin = A0;
+int sensorValue;
+int limit = 300;
+
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  int sensorValue = analogRead(soilSensor);
-  Serial.println(sensorValue);
-  delay(500);
+void loop() 
+{
+  sensorValue = analogRead(sensorPin);
+  Serial.println("Analoginen arvo : ");
+  //Serial.println(sensorValue);
+  
+  if (sensorValue<limit) 
+  {
+    digitalWrite (13, HIGH);
+    digitalWrite (11, LOW);
+    tone (12, 261);
+  }
+  else 
+    {
+    digitalWrite (13, LOW);
+    noTone (12);
+    digitalWrite (11, HIGH);
+    }
+  delay (1000);
+}    
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
 }
+
+void loop() 
+{
+  sensorValue = analogRead(sensorPin);
+  Serial.println("Analoginen arvo : ");
+  //Serial.println(sensorValue);
+  
+  if (sensorValue<limit) 
+  {
+    digitalWrite (13, HIGH);
+    digitalWrite (11, LOW);
+    tone (12, 261);
+  }
+  else 
+    {
+    digitalWrite (13, LOW);
+    noTone (12);
+    digitalWrite (11, HIGH);
+    }
+  delay (1000);
+}    
